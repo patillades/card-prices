f3227a323fc646476aa303033d6315dfe561730e
patillades <patillades@patillades-ubuntu.(none)>
2013-09-30 15:48:47 +0200

    canvis pq escrigui un arxiu amb els resultats del llistat

--- ex.js
+++ ex.js
@@ -1,66 +1,67 @@
 var MkmParser = function () {};
 
 MkmParser.prototype._host = 'https://es.magiccardmarket.eu/';
-MkmParser.prototype._cards = {};
-MkmParser.prototype._cardsDone = 0;
+MkmParser.prototype._cards = [];
 MkmParser.prototype._cardLimit;
-MkmParser.prototype._start;
 
 MkmParser.prototype.readFirstPage = function () {
+    console.log('a');
+    
     var page = require('webpage').create();
     var self = this;
-    
+    console.log('b');
     page.open(this._host + 'index.php?mainPage=browseUserProducts&idCategory=1&idUser=103227', function () {
 //    page.open(this._host + 'index.php?mainPage=browseUserProducts&idCategory=1&idUser=1854330', function () {
+        console.log('c');
         // dummy div
         var div = document.createElement('div');
         div.innerHTML = page.content;
 
         var cardRows = div.querySelectorAll('.MKMTable tbody tr');
-//        self._cardLimit = cardRows.length;
-        self._cardLimit = 10;
+        console.log(cardRows.length);
         
-        console.log('limit: ' + self._cardLimit);
-        
-        self._start = new Date().getTime();
+        self._cardLimit = cardRows.length;
         
         for(var i = 0; i < self._cardLimit; i++) {
-            var href = cardRows[i].children[2].children[0].href;
             var name = cardRows[i].children[2].children[0].innerHTML;
+            var href = this._host + cardRows[i].children[2].children[0].href;
+            
+            var langPattern = /showMsgBox\('[^']+/;
+            var lang = langPattern.exec(cardRows[i].children[6].innerHTML)[0];
+            lang = lang.substr("showMsgBox('".length, lang.length - "showMsgBox('".length);
+            
+            var condPattern = /cardstateicons\/\w{2}/;
+            var condition = condPattern.exec(cardRows[i].children[6].innerHTML)[0];
+            condition = condition.substr(condition.length - 2, 2);
+            
+            var foil = cardRows[i].children[7].innerHTML === '' ? false : true;
             var priceText = cardRows[i].children[9].innerHTML;
             
-            self._cards[name] = {
+            self._cards.push({
                 name: name,
                 href: href,
-                price: priceText,
-                avg: null
-            };
-            
-            console.log(i + ' ' + name);
-            
-            // l'error es produeix en la crida del timeout per als elements repetits
-            // ups, per als senars!?
-            
-            // que estrany, si faig el timeout esgraonat fallen els senars, si els llenço tots alhora després de X segons no falla cap
-            
-            (function (obj) {
-                // AIXO HO LLEGEIX SEMPRE
-                console.log(new Date().getTime() - obj.self._start + ': ' + 
-                    'pretimeout('+obj.i+'): ' + obj.card.name + '-' + obj.card.href + '-' + obj.card.price);
-            
-                setTimeout(function () {
-                    console.log(new Date().getTime() - obj.self._start + ': ' + 
-                        'timeout in: ' + obj.i);
-                    
-                    obj.self.readCardPage(obj.card, obj.i);
-
-                }, 2000*obj.i);
-            })({card: self._cards[name], i: i, self: self});
+                language: lang,
+                condition: condition,
+                foil: foil,
+                myPrice: priceText,
+            });
         }
-        
-        console.log('---');
-        
     });
+    
+    page.close();
+    
+    this.addCardsToFile();
+    
+    phantom.exit();
+};
+
+MkmParser.prototype.addCardsToFile = function () {
+    var fs = require('fs');
+    var file = 'list.json';
+
+    for(var i = 0; i < this._cards.length; i++) {
+        fs.write(file, this._cards[i], 'a');
+    }
 };
 
 MkmParser.prototype.readCardPage = function (card, i) {

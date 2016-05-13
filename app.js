d4e226a61f2d7bf1f8fce86081708acd6dd4f211
patillades <gorkamunya@hotmail.com>
2016-05-13 18:21:55 +0200

    iterate till all products are gotten

--- app.js
+++ app.js
@@ -6,15 +6,33 @@ var getUserProducts = require('./lib/getUserProducts');
 var HOST = 'https://es.magiccardmarket.eu/';
 
 var userCards = [];
+var pageNum = 0;
 
-// TODO: handle recursive calling of this func
-openProductsPage(HOST, userCards, 0, getUserProducts, function () {
-  console.log(userCards[0].name);
-  console.log(userCards[0].href);
-  console.log(userCards[0].language);
-  console.log(userCards[0].condition);
-  console.log(userCards[0].foil);
-  console.log(userCards[0].price);
-  
-  phantom.exit();
-});
+/**
+ * Keep getting the user's products while there are more pages
+ * 
+ * @param {WebPage} page - The page instance that has been closed
+ */
+function productsPageCloseCallback(page) {
+  if (page.hasNext) {
+    pageNum++;
+    
+    openProductsPage(HOST, userCards, pageNum, getUserProducts, productsPageCloseCallback);
+  } else {
+    console.log('cards length', userCards.length, '\n');
+    
+    for (var i=0; i < userCards.length; i++) {
+      console.log(userCards[i].name);
+      console.log(userCards[i].href);
+      console.log(userCards[i].language);
+      console.log(userCards[i].condition);
+      console.log(userCards[i].foil);
+      console.log(userCards[i].price);
+      console.log('---');
+    }
+    
+    phantom.exit();
+  }
+}
+
+openProductsPage(HOST, userCards, pageNum, getUserProducts, productsPageCloseCallback);

81d46ac2a89a1bbd9d3aa7368127571437b089b1
patillades <gorkamunya@hotmail.com>
2016-05-13 17:29:30 +0200

    some corrections

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* global phantom */
+
 var openProductsPage = require('./lib/openProductsPage');
 var getUserProducts = require('./lib/getUserProducts');
 
@@ -6,4 +8,13 @@ var HOST = 'https://es.magiccardmarket.eu/';
 var userCards = [];
 
 // TODO: handle recursive calling of this func
-openProductsPage(HOST, userCards, 0, getUserProducts);
+openProductsPage(HOST, userCards, 0, getUserProducts, function () {
+  console.log(userCards[0].name);
+  console.log(userCards[0].href);
+  console.log(userCards[0].language);
+  console.log(userCards[0].condition);
+  console.log(userCards[0].foil);
+  console.log(userCards[0].price);
+  
+  phantom.exit();
+});

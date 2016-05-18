d46f41942f0fa09f156b5cd38a5437771e2f1417
patillades <gorkamunya@hotmail.com>
2016-05-18 17:01:04 +0200

    use async to read the product list pages

--- app.js
+++ app.js
@@ -9,22 +9,41 @@ var openCardPage = require('./lib/openCardPage');
 var userCards = [];
 var pageNum = 0;
 
-/**
- * Keep getting the user's products while there are more pages
- * 
- * @param {WebPage} page - The page instance that has been closed
- */
-function productsPageCloseCallback(page) {
-  // check for a "next page" link to know whether to keep paginating 
-  // (faster than turning into DOM and querying for the <a> element)
-  if (page.content.indexOf('rel="next"') !== -1) {
-    pageNum++;
-    
-    return openProductsByPage(pageNum);
-  }
-  
-  console.log('The user has', userCards.length, 'cards\n');
+// Keep getting the user's products while there are more pages
+async.doWhilst(
+  /**
+   * Get the products from a user's page
+   * 
+   * @param {Function} callback - A function which must be called once openProductsPage has completed
+   */
+  function getProducts(callback) {
+    openProductsPage(userCards, callback, pageNum);
+  },
+  /**
+   * Check for a "next page" link to know whether to keep paginating 
+   * 
+   * @param {WebPage} page - The page instance that has been closed
+   * @returns {Boolean}
+   */
+  function hasNext(page) {
+    // indexOf is faster than turning into DOM and querying for the <a> element
+    if (page.content.indexOf('rel="next"') !== -1) {
+      pageNum++;
 
+      return true;
+    }
+
+    return false;
+  },
+  allProductsGotten
+);
+
+/**
+ * Once all the product list pages are read, get the products' price trends
+ */
+function allProductsGotten() {
+  console.info('The user has', userCards.length, 'cards\n');
+  
   async.mapSeries(userCards, openCardPage, function mapCb(err, pricedCards) {
     if (err) {
       console.error('mapSeries err', err);
@@ -48,7 +67,3 @@ function productsPageCloseCallback(page) {
     phantom.exit();
   });
 }
-
-var openProductsByPage = openProductsPage.bind(null, userCards, productsPageCloseCallback);
-
-openProductsByPage(pageNum);
